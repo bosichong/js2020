0f4b342d359dad918c4c73ece2d50d7d6be71023
J_sky <bosichong@qq.com>
2019-12-07 21:27:02 +0800

    这是一个快速学习JavaScript的学习笔记

--- js/main.js
+++ js/main.js
@@ -9,22 +9,33 @@ console.log("这是一个快速学习JavaScript的学习笔记"); //控制态输
 // 导航
 document.getElementById("lt").innerHTML = '<ul>'+'<li><a href="#m1">JavaScript简介</a>' +
     '<li><a href="#m2">JavaScript开发环境</a>'+
+    '<li><a href="#m3">推荐一些VS code的JS相关插件</a>'+
     '</ul>';
 
 
 //正文
-document.getElementById("rt").innerHTML = '<h1>这是一个快速学习JavaScript的学习笔记</h1>' +
+document.getElementById("rt").innerHTML = '<h2>这是一个快速学习JavaScript的学习笔记</h2>' +
     '</br>' +
-    '<h2 id="m1">JavaScript 是脚本语言</h2>' +
+    '<h3 id="m1">JavaScript 是脚本语言</h3>' +
     '<p>JavaScript 是一种轻量级的编程语言。\
     <p>JavaScript 是可插入 HTML 页面的编程代码。\
     <p>JavaScript 插入 HTML 页面后，可由所有的现代浏览器执行。\
     <p>JavaScript 很容易学习。' +
-    '<h2 id="m2">JavaScript开发环境</h2>' +
+    '<h3 id="m2">JavaScript开发环境</h3>' +
     '<p>以前我在网上看到一些初级编程的教程，经常有人写到：新手敲代码要用记事本手打，这样学习印象深刻！' +
     '<p>用记事本敲代码真的能是人印象深刻？但是我想愉快的学习不可以吗？答案当然是True了。' +
-    "<p>个人目前认为最好用的JavaScript IDE 就是：Visual Studio Code，简称VS code"+
-    wrmk('![其实这不是站长的本人的照片](./img/Snip20191207_2.png)');
+    '<p>个人目前认为最好用的JavaScript IDE 就是：Visual Studio Code，简称VS code'+
+    wrmk('![](./img/Snip20191207_2.png)')+
+    '<h3 id="m3">推荐一些VS code的JS相关插件</h3>'+
+    wrmk('![](./img/Snip20191207_3.png)')+
+    '插件的一些功能基本为：代码美化，HTML JavaScript代码片段及提示\
+    其中Live Server 是一个实时简易web服务器，用来预览HTML和JS很方便了，\
+    浏览器推荐Firefox'+
+    wrmk('![](./img/Snip20191207_4.png)')+
+    '好了，有了这些好工具，我们可以开始愉快的学习JavaScript了！'
+
+    
+
 
 
 
